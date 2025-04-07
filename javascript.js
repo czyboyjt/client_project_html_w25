7b6514b2e615a7e516642be2ddfa24c45a6a2078
Jonte Taffe <jtaffe@umich.edu>
2025-04-06 20:38:20 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,1 +1,8 @@
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+let submit = document.getElementById("submit")
+submit.addEventListener("click", function (e) {
+    alert("Form submitted!");
+    e.preventDefault();
+    let form = document.getElementById("form");
+    form.reset();
+})
